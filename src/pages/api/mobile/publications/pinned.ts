fd0634a24e386dba2a32d4606fab3a92af30972d
oussamachah2020 <otaksam213@gmail.com>
2025-01-09 10:14:11 +0100

    feat: endpoint to get 5 first pinned publications

---
+++ src/pages/api/mobile/publications/pinned.ts
@@ -0,0 +1,26 @@
+import { authMiddleware } from "@/apiMiddleware";
+import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const prisma = new PrismaClient();
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "GET") {
+    return res.status(405).json({ message: "Method Not Allowed" });
+  }
+
+  try {
+    const pinnedPublications = await prisma.publication.findMany({
+      where: { pinned: true },
+      orderBy: { createdAt: "desc" },
+      take: 5,
+    });
+
+    return res.status(200).json(pinnedPublications);
+  } catch (error) {
+    console.error("Error fetching pinned publications:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
+export default authMiddleware(handler);
